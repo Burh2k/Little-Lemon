5e22184a9b193b0c20eabe2ec7c8c9398ff78a0e
Muhammad Burhan Ahmed <burhan.ahmed60090@gmail.com>
2023-08-03 23:08:02 +0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Little-Lemon",
   build: {
     rollupOptions: {
       external: ['conversion.js'],
